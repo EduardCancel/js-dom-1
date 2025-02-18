d1d928306080b25fc31b27f2bd10701cca549cdd
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-18 16:00:02 +0100

    add functions to button that turns on the light

---
+++ main.js
@@ -0,0 +1,11 @@
+const lightButton = document.querySelector('button')
+
+lightButton.addEventListener('click', function(){
+    console.log('click button');
+    const lightEl = document.getElementById("light")
+    lightEl.src ="../img-7/yellow_lamp.png"
+})
+
+
+
+
